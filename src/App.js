a518a300492df6996fc0c65c37141fde535c7113
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2022-06-10 16:50:16 +0545

    dynamic input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [inputFields, setInputFields] = useState([{ name: "", age: "" }]);
+    
+  const handleFormChange = (index, event) => {
+    let data = [...inputFields];
+    data[index][event.target.name] = event.target.value;
+    setInputFields(data);
+  };
+
+  const addFields = () => {
+    let newField = { name: "", age: "" };
+    setInputFields([...inputFields, newField]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        {inputFields.map((input, index) => {
+          return (
+            <div key={index}>
+              <input
+                name="name"
+                placeholder="Name"
+                value={input.name}
+                onChange={(event) => handleFormChange(index, event)}
+              />
+              <input
+                name="age"
+                placeholder="Age"
+                value={input.age}
+                onChange={(event) => handleFormChange(index, event)}
+              />
+            </div>
+          );
+        })}
+      </form>
+      <button onClick={addFields}>Add more</button>
     </div>
   );
 }
